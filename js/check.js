8e55c54d77071b24a16c22b0190833e8920584f5
VitaliiChichuga <chichygav@gmail.com>
2023-07-31 14:13:39 +0300

    add item

---
+++ js/check.js
@@ -0,0 +1,104 @@
+const getHref = document.querySelector('.dashboard-title-link').href;
+const getLinkForInnerText = document.querySelector('a.dashboard-title-link');
+
+const copyLinkBtn = document.querySelector('.dashboard-title-copy-link');
+const copyLinkBtnText = document.querySelector(
+  '.dashboard-title-copy-link-text',
+);
+const btnMenu = document.querySelectorAll('.dashboard-btn-menu');
+const btnMenuShow = document.querySelector('.dashboard-btn-menu-show');
+const linkText = document.querySelector('.dashboard-title-link');
+const checkAll = document.getElementById('checked-all');
+const checkBoxes = document.querySelectorAll(
+  'input.dashboard-input-checked-card',
+);
+const spanBtnWorksheets = document.querySelectorAll('.span-btn-worksheets');
+const spanBtnImages = document.querySelectorAll('.span-btn-images');
+const spanBtnAll = document.querySelectorAll('.span-btn-all');
+const spanBtnDelete = document.querySelectorAll('.span-btn-delete');
+const dashboardItemCardImg = document.querySelectorAll(
+  '.dashboard-item-card__img',
+);
+
+// ========Displays the link text based on the link
+function displaysLinkTextBasedOnLink() {
+  getLinkForInnerText.textContent = getHref;
+}
+
+// ======Show escape text "Copied" when clicking CopyLink button=====
+copyLinkBtn.onclick = function () {
+  copyLinkBtnText.classList.add('copy-link-active');
+  setTimeout(() => copyLinkBtnText.classList.remove('copy-link-active'), 2000);
+  // ==========Copy link on button click
+  navigator.clipboard.writeText(linkText.innerHTML);
+};
+
+// ======Selecting one or all checkboxes======
+for (let i = 0; i < checkBoxes.length; i++) {
+  checkBoxes[i].onclick = function () {
+    const checkedCount = document.querySelectorAll(
+      'input.dashboard-input-checked-card:checked',
+    ).length;
+
+    checkAll.checked = checkedCount > 0;
+    checkAll.indeterminate =
+      checkedCount > 0 && checkedCount < checkBoxes.length;
+
+    for (const spanBtnWorksheet of spanBtnWorksheets) {
+      spanBtnWorksheet.textContent = checkedCount + ' ';
+    }
+
+    for (const spanBtnImage of spanBtnImages) {
+      spanBtnImage.textContent = checkedCount + ' ';
+    }
+    for (const spanBtnAllItem of spanBtnAll) {
+      spanBtnAllItem.textContent = checkedCount + checkedCount + ' ';
+    }
+    for (const spanBtnDeleteItem of spanBtnDelete) {
+      spanBtnDeleteItem.textContent = checkedCount + ' ';
+    }
+
+    // ======Show the "dashboard-btn-menu" if at least one checkbox is selected
+    for (const el of btnMenu) {
+      el.classList.add('dashboard-btn-menu-show');
+      if (checkedCount === 0) {
+        el.classList.remove('dashboard-btn-menu-show');
+      }
+    }
+  };
+}
+
+checkAll.onclick = function () {
+  for (let i = 0; i < checkBoxes.length; i++) {
+    checkBoxes[i].checked = this.checked;
+    const checkedCount = document.querySelectorAll(
+      'input.dashboard-input-checked-card:checked',
+    ).length;
+
+    for (const spanBtnWorksheet of spanBtnWorksheets) {
+      spanBtnWorksheet.textContent = checkedCount + ' ';
+    }
+
+    for (const spanBtnImage of spanBtnImages) {
+      spanBtnImage.textContent = checkedCount + ' ';
+    }
+    for (const spanBtnAllItem of spanBtnAll) {
+      spanBtnAllItem.textContent = checkedCount + checkedCount + ' ';
+    }
+    for (const spanBtnDeleteItem of spanBtnDelete) {
+      spanBtnDeleteItem.textContent = checkedCount + ' ';
+    }
+
+    // ========Show "dashboard-btn-menu" if all checkboxes are selected
+    for (const el of btnMenu) {
+      el.classList.add('dashboard-btn-menu-show');
+      if (this.checked === false) {
+        el.classList.remove('dashboard-btn-menu-show');
+      }
+    }
+  }
+};
+
+// ==========
+
+displaysLinkTextBasedOnLink();
