9560d0ece71b753f1af48bccc5237349bfbfd48d
VitaliiChichuga <chichygav@gmail.com>
2023-08-05 13:59:49 +0300

    add modal

---
+++ js/modal-img.js
@@ -0,0 +1,21 @@
+const modal = document.querySelector('dialog');
+const modalContent = document.querySelector('.modal__content');
+const dashboardList = document.querySelector('.dashboard-list ');
+
+dashboardList.addEventListener('click', showModal);
+
+function showModal(e) {
+  let elem = e.target.parentElement;
+  if (!elem.classList.contains('dashboard-item-card__thumb')) {
+    return;
+  }
+
+  modalContent.innerHTML = elem.outerHTML;
+  modal.showModal();
+}
+
+modal.onclick = e => {
+  if (!e.target.closest('.modal__content')) {
+    modal.close();
+  }
+};
