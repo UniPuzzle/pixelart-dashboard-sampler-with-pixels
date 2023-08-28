8d1f4aa0db4301cd0c382121ce24f8af7d38bb39
VitaliiChichuga <chichygav@gmail.com>
2023-08-28 10:57:22 +0300

    Arrangement of cards sample 1

--- js/modal-img.js
+++ js/modal-img.js
@@ -1,3 +1,28 @@
+// const modal = document.querySelector('dialog');
+// const modalContent = document.querySelector('.modal__content');
+// const dashboardList = document.querySelector('.dashboard-list ');
+
+// dashboardList.addEventListener('click', showModal);
+
+// function showModal(e) {
+//   let elem = e.target.parentElement;
+//   if (
+//     !elem.classList.contains('dashboard-item-card__thumb') &&
+//     !elem.classList.contains('dashboard-item-card__thumb--horizontal')
+//   ) {
+//     return;
+//   }
+
+//   modalContent.innerHTML = elem.outerHTML;
+//   modal.showModal();
+// }
+
+// modal.onclick = e => {
+//   if (!e.target.closest('.modal__content')) {
+//     modal.close();
+//   }
+// };
+
 const modal = document.querySelector('dialog');
 const modalContent = document.querySelector('.modal__content');
 const dashboardList = document.querySelector('.dashboard-list ');
@@ -13,12 +38,15 @@ function showModal(e) {
     return;
   }
 
-  modalContent.innerHTML = elem.outerHTML;
+  modal.innerHTML = elem.outerHTML;
   modal.showModal();
 }
 
 modal.onclick = e => {
-  if (!e.target.closest('.modal__content')) {
+  if (
+    !e.target.closest('.dashboard-item-card__thumb') &&
+    !e.target.closest('.dashboard-item-card__thumb--horizontal')
+  ) {
     modal.close();
   }
 };
